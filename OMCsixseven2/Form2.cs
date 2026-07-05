3c9884719577b8dea66bc48f8f0d61f8c2720e88
SITI SOFEA BINTI AHMAD FAZIL <siti_25010287@utp.edu.my>
2026-07-05 14:40:53 +0800

    button

---
+++ OMCsixseven2/Form2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OMCsixseven2
+{
+    public partial class homepage : Form
+    {
+        public homepage()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
