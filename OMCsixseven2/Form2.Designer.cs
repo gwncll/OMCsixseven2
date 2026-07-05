07c5a94c6f7613deca57c6edb318b3a7b3ce1c32
SITI SOFEA BINTI AHMAD FAZIL <siti_25010287@utp.edu.my>
2026-07-05 14:41:22 +0800

    hi

--- OMCsixseven2/Form2.Designer.cs
+++ OMCsixseven2/Form2.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -40,11 +42,31 @@
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            button2.Location = new Point(312, 242);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(397, 141);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
             // homepage
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(971, 542);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "homepage";
             Text = "Form2";
@@ -55,5 +77,7 @@
         #endregion
 
         private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
