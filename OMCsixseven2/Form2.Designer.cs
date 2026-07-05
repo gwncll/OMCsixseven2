2d906caebdb85ac566b6e6761070d30d24989837
ADLIN SHAHIRA MUHAMMAD BAHARUDDIN <adlin_25009596@utp.edu.my>
2026-07-05 14:48:19 +0800

    added a button

--- OMCsixseven2/Form2.Designer.cs
+++ OMCsixseven2/Form2.Designer.cs
@@ -30,6 +30,7 @@
         {
             button1 = new Button();
             button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -50,11 +51,21 @@
             button2.Text = "button2";
             button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            button3.Location = new Point(510, 197);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form2";
@@ -66,5 +77,6 @@
 
         private Button button1;
         private Button button2;
+        private Button button3;
     }
 }
